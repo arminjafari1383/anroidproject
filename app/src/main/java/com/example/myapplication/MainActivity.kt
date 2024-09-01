a11a2f140259d73846dc3bbdc426fe38a31ee66f
unknown <arminjafri138386@gmail.com>
2024-08-31 23:09:24 -0700

    update for on backed pressed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,27 +12,30 @@ import android.util.Log
 import android.view.View
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private var doubleclick = false
+    private  var exit = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-    }
+        onBackPressedDispatcher.addCallback(
+            this,
+            object :OnBackPressedCallback(true){
+                override fun handleOnBackPressed() {
+                    if (exit)
+                        finish()
+                    exit = true
+                    toast("برای خروج دوبار کلیک کنید")
+                    Handler(Looper.getMainLooper()).postDelayed({exit = false},2000)
+                }
 
-    override fun onBackPressed() {
-        if (doubleclick){
-            super.onBackPressed()
-            return
-        }
-        doubleclick = true
-        toast("برای خروح دو بار کلیک کنید")
-
-        Handler(Looper.getMainLooper()).postDelayed({doubleclick = false},2000)
+            }
+        )
     }
     private fun toast(text: String) {
         Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
