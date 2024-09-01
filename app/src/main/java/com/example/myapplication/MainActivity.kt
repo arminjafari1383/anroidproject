976d64ebb5c17a28d82e142787eec8fda4dc37f1
unknown <arminjafri138386@gmail.com>
2024-08-31 22:51:12 -0700

    on back pressed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.SharedPreferences
 import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.SeekBar
@@ -14,26 +16,25 @@ import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private var doubleclick = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        toast("oncreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        toast("onStart")
-    }
+    override fun onBackPressed() {
+        if (doubleclick){
+            super.onBackPressed()
+            return
+        }
+        doubleclick = true
+        toast("برای خروح دو بار کلیک کنید")
 
+        Handler(Looper.getMainLooper()).postDelayed({doubleclick = false},2000)
+    }
     private fun toast(text: String) {
         Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
     }
-
-    override fun onResume() {
-        super.onResume()
-        toast("onResume")
-    }
-
 }
